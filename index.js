d23d22fcff25084e1f8264b6398cb6217976b0c7
mercy cherono <mcherono529@gmail.com>
2024-09-29 20:09:25 +0300

    adds arrow functions

---
+++ index.js
@@ -0,0 +1,17 @@
+// Function expression for divide
+const divide = function(a, b) {
+    return a / b;
+};
+
+// Arrow function for square
+const square = (x) => {
+    return x * x;
+};
+
+// Arrow function for add
+const add = (a, b) => {
+    return a + b;
+};
+
+// Export the functions if needed for testing
+module.exports = { divide, square, add };
